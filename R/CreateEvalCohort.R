1609bfedf1b281d4944f2302b8b9f5067d58546c
schuemie <schuemie@ohdsi.org>
2020-03-25 05:08:47 -0400

    Making sure code also works if covariate settings is not a list

--- R/CreateEvalCohort.R
+++ R/CreateEvalCohort.R
@@ -185,7 +185,8 @@ createEvaluationCohort <- function(connectionDetails,
   # will only use the covariates with non-zero betas
   lrNonZeroCovs <- c(lr_results$model$varImp$covariateId[lr_results$model$varImp$covariateValue !=
                                                            0])
-  #covariateSettings$includedCovariateIds <- lrNonZeroCovs
+  if (is(covariateSettings, "covariateSettings"))
+    covariateSettings <- list(covariateSettings)
 
   for(listUp in 1:length(covariateSettings)) {
     covariateSettings[[listUp]]$includedCovariateIds <- c(lrNonZeroCovs)
