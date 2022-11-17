84cd85589a9a71804aa2a5b7273a029286b5f120
Schuemie <MSCHUEMI@its.jnj.com>
2022-11-17 02:50:26 -0500

    Cleaning up test code

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,58 +1,42 @@
-#' utility function to make sure connection is closed after usage
-with_dbc_connection <- function(connection, code) {
-  on.exit({
-    DatabaseConnector::disconnect(connection)
-  })
-  eval(substitute(code), envir = connection, enclos = parent.frame())
+library(Eunomia)
+connectionDetails <- getEunomiaConnectionDetails()
+
+if (grepl("testthat", getwd())) {
+  cohortPath <- "cohorts"
+} else {
+  cohortPath <- file.path("tests", "testthat", "cohorts")
 }
 
-#' Only works with postgres > 9.4
-.pgTableExists <- function(connection, schema, tableName) {
-  return(!is.na(
-    DatabaseConnector::renderTranslateQuerySql(
-      connection,
-      "SELECT to_regclass('@schema.@table');",
-      table = tableName,
-      schema = schema
-    )
-  )[[1]])
-}
+creationFile <- file.path(cohortPath, "CohortsToCreate.csv")
+cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(
+  settingsFileName = creationFile,
+  sqlFolder = cohortPath,
+  jsonFolder = cohortPath,
+  cohortFileNameValue = c("cohortId")
+)
 
-# Create a cohort definition set from test cohorts
-createCohortSet <- function(connectionDetails = NULL) {
+cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = "cohort")
 
-  if (grepl("testthat", getwd())) {
-    cohortPath <- "cohorts"
-  } else {
-    cohortPath <- file.path("tests", "testthat", "cohorts")
-  }
-
-  creationFile <- file.path(cohortPath, "CohortsToCreate.csv")
-  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(
-    settingsFileName = creationFile,
-    sqlFolder = cohortPath,
-    jsonFolder = cohortPath,
-    cohortFileNameValue = c("cohortId")
-  )
-
-  cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = "cohort")
-
-  CohortGenerator::createCohortTables(connectionDetails = connectionDetails,
-                                      cohortTableNames = cohortTableNames,
-                                      cohortDatabaseSchema = "main",
-                                      incremental = FALSE)
+CohortGenerator::createCohortTables(
+  connectionDetails = connectionDetails,
+  cohortTableNames = cohortTableNames,
+  cohortDatabaseSchema = "main",
+  incremental = FALSE
+)
 
 
-  cohortDefinitionSet$checksum <- CohortGenerator::computeChecksum(cohortDefinitionSet$sql)
+CohortGenerator::generateCohortSet(
+  connectionDetails = connectionDetails,
+  cdmDatabaseSchema = "main",
+  cohortDatabaseSchema = "main",
+  cohortTableNames = cohortTableNames,
+  cohortDefinitionSet = cohortDefinitionSet,
+  incremental = FALSE
+)
 
-  cohortsGenerated <- CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
-                                                         cdmDatabaseSchema = "main",
-                                                         cohortDatabaseSchema = "main",
-                                                         cohortTableNames = cohortTableNames,
-                                                         cohortDefinitionSet = cohortDefinitionSet,
-                                                         incremental = FALSE)
-
-
-  return
-}
-
+withr::defer(
+  {
+    unlink(connectionDetails$server())
+  },
+  testthat::teardown_env()
+)
--- tests/testthat/test_EndToEnd.R
+++ tests/testthat/test_EndToEnd.R
@@ -2,116 +2,85 @@ library(PheValuator)
 library(testthat)
 library(Eunomia)
 
-runTest <- function(folder, databaseId) {
-  connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+test_that("TestPhenotype - test PheValuator end to end", {
+  folder <- tempfile("Phevaluator")
+  dir.create(folder)
+  on.exit(unlink(folder, recursive = TRUE))
 
-  #databases are in the format:
-  databaseDetails <- list(databaseId = databaseId,
-                          cdmDatabaseSchema = "main",
-                          cohortDatabaseSchema = "main",
-                          cohortTable = "cohort",
-                          workDatabaseSchema = "main",
-                          connectionDetails = connectionDetails)
-
-
-  dbList <- list(databaseDetails) #insert the db's you want to run in this list
-
-  cohorts <- createCohortSet(connectionDetails)
-
-  #create the 4 cohorts below needed for the analysis
+  databaseId <- "Eunomia"
+  cdmDatabaseSchema <- "main"
+  cohortDatabaseSchema <- "main"
+  cohortTable <- "cohort"
+  workDatabaseSchema <- "main"
   xSpecCohort <- 1001
   excludedCovariateConceptIds <- c()
   xSensCohort <- 2
   prevalenceCohort <- 2
   evaluationPopulationCohortId <- 5
 
-  # Create analysis settings ----------------------------------------------------------------------------------------
-  CovSettingsAcute <- createDefaultCovariateSettings(excludedCovariateConceptIds = excludedCovariateConceptIds,
-                                                     addDescendantsToExclude = TRUE,
-                                                     startDayWindow1 = 0,
-                                                     endDayWindow1 = 10,
-                                                     startDayWindow2 = 11,
-                                                     endDayWindow2 = 20,
-                                                     startDayWindow3 = 21,
-                                                     endDayWindow3 = 9999)
+  # Create analysis settings ---------------------------------------------------
+  CovSettingsAcute <- createDefaultCovariateSettings(
+    excludedCovariateConceptIds = excludedCovariateConceptIds,
+    addDescendantsToExclude = TRUE,
+    startDayWindow1 = 0,
+    endDayWindow1 = 10,
+    startDayWindow2 = 11,
+    endDayWindow2 = 20,
+    startDayWindow3 = 21,
+    endDayWindow3 = 9999
+  )
 
-  CohortArgsAcute <- createCreateEvaluationCohortArgs(xSpecCohortId = xSpecCohort,
-                                                      xSensCohortId = xSensCohort,
-                                                      prevalenceCohortId = prevalenceCohort,
-                                                      evaluationPopulationCohortId = evaluationPopulationCohortId,
-                                                      evaluationPopulationCohortIdStartDay = 0,
-                                                      evaluationPopulationCohortIdEndDay = 0,
-                                                      modelBaseSampleSize = 15000,
-                                                      xSpecCohortSize = 5000,
-                                                      covariateSettings = CovSettingsAcute,
-                                                      baseSampleSize = 2000000,
-                                                      startDate = "19000101", #can change these dates to suit your database
-                                                      endDate = "21000101",
-                                                      saveEvaluationCohortPlpData = TRUE,
-                                                      excludeModelFromEvaluation = TRUE)
+  CohortArgsAcute <- createCreateEvaluationCohortArgs(
+    xSpecCohortId = xSpecCohort,
+    xSensCohortId = xSensCohort,
+    prevalenceCohortId = prevalenceCohort,
+    evaluationPopulationCohortId = evaluationPopulationCohortId,
+    evaluationPopulationCohortIdStartDay = 0,
+    evaluationPopulationCohortIdEndDay = 0,
+    modelBaseSampleSize = 15000,
+    xSpecCohortSize = 5000,
+    covariateSettings = CovSettingsAcute,
+    baseSampleSize = 2000000,
+    startDate = "19000101",
+    endDate = "21000101",
+    saveEvaluationCohortPlpData = TRUE,
+    excludeModelFromEvaluation = TRUE
+  )
+
+  conditionAlgTestArgs <- createTestPhenotypeAlgorithmArgs(
+    cutPoints = c("EV", 0.1),
+    phenotypeCohortId = 6,
+    washoutPeriod = 0
+  )
+
+  analysis1 <- createPheValuatorAnalysis(
+    analysisId = 1,
+    description = "6",
+    createEvaluationCohortArgs = CohortArgsAcute,
+    testPhenotypeAlgorithmArgs = conditionAlgTestArgs
+  )
 
 
-  ##### First phenotype algorithm to test ##############
-  conditionAlgTestArgs <- createTestPhenotypeAlgorithmArgs(cutPoints = c("EV", 0.1),
-                                                           phenotypeCohortId =  6, #CHANGE THIS: 1st phenotype algorithm to test
-                                                           washoutPeriod = 0) #CHANGE THIS: to how many continuous observation days prior to index (e.g., 365)
-
-  analysis1 <- createPheValuatorAnalysis(analysisId = 1,
-                                         description = "6", #CHANGE THIS: to a good name for your phenotype algorithm to test
-                                         createEvaluationCohortArgs = CohortArgsAcute,
-                                         testPhenotypeAlgorithmArgs = conditionAlgTestArgs)
-
-
-  pheValuatorAnalysisList <- list(analysis1) #add any additional phenotypes to test to this list, e.g., analysis3
+  pheValuatorAnalysisList <- list(analysis1)
 
   savePheValuatorAnalysisList(pheValuatorAnalysisList, file.path(folder, "pheValuatorAnalysisSettings.json"))
 
-  for (dbUp in 1:length(dbList)) {
+  # Run analysis ---------------------------------------------------------------
+  pheValuatorAnalysisList <- loadPheValuatorAnalysisList(file.path(folder, "pheValuatorAnalysisSettings.json"))
 
-    pheValuatorAnalysisList <- loadPheValuatorAnalysisList(file.path(folder, "pheValuatorAnalysisSettings.json"))
+  referenceTable <- runPheValuatorAnalyses(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    workDatabaseSchema = workDatabaseSchema,
+    outputFolder = folder,
+    pheValuatorAnalysisList = pheValuatorAnalysisList
+  )
 
-    outFolder <- file.path(folder, paste0(dbList[[dbUp]]$databaseId))
+  expect_true(file.exists(file.path(folder, "TestResults_a1.rds")))
 
-    referenceTable <- runPheValuatorAnalyses(connectionDetails = dbList[[dbUp]]$connectionDetails,
-                                             cdmDatabaseSchema = dbList[[dbUp]]$cdmDatabaseSchema,
-                                             cohortDatabaseSchema = dbList[[dbUp]]$cohortDatabaseSchema,
-                                             cohortTable = dbList[[dbUp]]$cohortTable,
-                                             workDatabaseSchema = dbList[[dbUp]]$workDatabaseSchema,
-                                             outputFolder = outFolder,
-                                             pheValuatorAnalysisList = pheValuatorAnalysisList)
-
-  }
-}
-
-test_that("TestPhenotype - test PheValuator end to end", {
-  fail <- 0
-
-  while(fail < 3) {
-    unlink(tempdir(), recursive = TRUE)
-
-    Sys.setenv(EUNOMIA_DATA_FOLDER = tempdir())
-
-    andromedaTempFolder <- tempdir() #change to a spot where you have some disk space for temporary files
-
-    options(andromedaTempFolder = andromedaTempFolder)
-
-    folder <- tempdir() #change to a spot where you have significant free space
-
-    databaseId <- "Eunomia"
-    results <- runTest(folder = folder, databaseId = databaseId)
-
-    if(file.exists(file.path(folder, databaseId, "TestResults_a1.rds"))) {
-      break
-    } else {
-      fail <- fail + 1
-    }
-  }
-
-  expect_true(file.exists(file.path(folder, databaseId, "TestResults_a1.rds")))
-
-  outputFile <- readRDS(file.path(folder, databaseId, "TestResults_a1.rds"))
+  outputFile <- readRDS(file.path(folder, "TestResults_a1.rds"))
 
   expect_true(as.numeric(outputFile$sensitivity[[1]]) >= 0.1 & as.numeric(outputFile$sensitivity[[1]]) <= 0.9)
-
 })
-
