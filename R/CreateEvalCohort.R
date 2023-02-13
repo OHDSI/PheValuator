14d2b11ef955b5434999125fd5d10e0de45365da
Joel swerdel <jswerdel@its.jnj.com>
2023-02-13 13:16:26 +0000

    - change prevalence

--- R/CreateEvalCohort.R
+++ R/CreateEvalCohort.R
@@ -135,7 +135,7 @@
         ParallelLogger::logInfo("Creating evaluation cohort subjects excluding visits from cohort Id: ", exclusionEvaluationCohortId)
       }
 
-      if(prevalenceCohortId < 1) {prevalenceCohortId <= 1001}
+      if(prevalenceCohortId < 1) {prevalenceCohortId <= 5}
 
       sql <- SqlRender::loadRenderTranslateSql(
         sqlFilename = sqlFilename,
