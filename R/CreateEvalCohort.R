1185f9cc27ff2ee53f477bad956259e6c1e1acd2
O'Brien, William [JRDUS] <Wobrien8@its.jnj.com>
2026-08-13 14:29:25 -0400

    passes cran checks

--- R/CreateEvalCohort.R
+++ R/CreateEvalCohort.R
@@ -14,6 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+utils::globalVariables(c(
+  "subjectId",
+  "cohortStartDate",
+  "lowDate",
+  "highDate"
+))
+
 .createEvaluationCohort <- function(connectionDetails,
                                     phenotype,
                                     analysisName,
